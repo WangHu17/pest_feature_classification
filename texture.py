b3faa908e77196603befad80f99e0419f0cec09b
wanghu <1936749601@qq.com>
2022-08-27 22:37:52 +0800

    texture

--- texture.py
+++ texture.py
@@ -3,7 +3,47 @@ import svm
 
 
 def texture_classify(path):
-    return svm.predict(path, "texture")
+    res = svm.predict(path, "texture")
+    res = str(res)
+    res = res[2:-2]
+    if res == 'under':
+        return '仰姿'
+    elif res == '0':
+        return '八字地老虎'
+    elif res == '1':
+        return '扁刺蛾'
+    elif res == '2':
+        return '甘蓝夜蛾'
+    elif res == '3':
+        return '甘暮天蛾'
+    elif res == '4':
+        return '黄地老虎'
+    elif res == '5':
+        return '金龟子'
+    elif res == '6':
+        return '愧尺蠖'
+    elif res == '7':
+        return '六点天蛾'
+    elif res == '8':
+        return '棉铃虫'
+    elif res == '9':
+        return '粘虫'
+    elif res == '10':
+        return '深色白眉天蛾'
+    elif res == '11':
+        return '小地老虎'
+    elif res == '12':
+        return '银锭夜蛾'
+    elif res == '13':
+        return '榆绿天蛾'
+    elif res == '14':
+        return '未定种'
+    elif res == '15':
+        return '美国白蛾'
+    elif res == '16':
+        return '旋幽夜蛾1'
+    elif res == '17':
+        return '旋幽夜蛾2'
 
 
 if __name__ == '__main__':
