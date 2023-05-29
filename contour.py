ab1d95f99e7cedcc5fc2b0e56624e189dc42ecfd
wanghu <1936749601@qq.com>
2023-05-29 16:55:57 +0800

    20230529

--- contour.py
+++ contour.py
@@ -15,33 +15,33 @@ def contour_classify(path):
     elif res == 'xiaodian':
         return '小点形'
     elif res == 'diechi':
-        return '蝶翅形'
+        return '展翅形'
     elif res == 'dachi':
         return '大翅形'
     elif res == 'zhanchi':
-        return '展翅形'
+        return '上扬翅形'
     elif res == 'yi':
         return '异形'
 
 
 if __name__ == '__main__':
-    # for i in range(1, len(sys.argv)):
-    #     print(contour_classify(sys.argv[i]))
+    for i in range(1, len(sys.argv)):
+        print(contour_classify(sys.argv[i]))
     # path = r'F:\DataSet\contour_train_imgs\changtiao\ct (2).jpg'
     # print(contour_classify(path))
-    print("程序正在运行...")
-    T1 = time.time()
-    root = "F:\\DataSet\\testImages1"
-    dirs = os.listdir(root)
-    for dir in dirs:
-        path = root + "\\" + dir
-        files = os.listdir(path)
-        index = 0
-        for file in files:
-            index = index + 1
-            filePath = root + "\\" + dir + "\\" + file
-            resColor = contour_classify(filePath)
-            newName = root + "\\" + dir + "\\" + resColor + str(index) + ".jpg"
-            os.rename(filePath, newName)
-    T2 = time.time()
-    print('程序运行时间为: {:.2f}秒'.format(T2 - T1))
+    # print("程序正在运行...")
+    # T1 = time.time()
+    # root = "F:\\DataSet\\testImages1"
+    # dirs = os.listdir(root)
+    # for dir in dirs:
+    #     path = root + "\\" + dir
+    #     files = os.listdir(path)
+    #     index = 0
+    #     for file in files:
+    #         index = index + 1
+    #         filePath = root + "\\" + dir + "\\" + file
+    #         resColor = contour_classify(filePath)
+    #         newName = root + "\\" + dir + "\\" + resColor + str(index) + ".jpg"
+    #         os.rename(filePath, newName)
+    # T2 = time.time()
+    # print('程序运行时间为: {:.2f}秒'.format(T2 - T1))
